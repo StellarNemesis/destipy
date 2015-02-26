0d935eff3906dea491489fa349eafd6840fa25a0
erm <j@ermff.com>
2015-02-26 23:25:55 +1100

    changing a variable name, passing membership id to methods instead of through config

--- destiny.py
+++ destiny.py
@@ -6,27 +6,28 @@ class DestinyAPI(object):
 
     def __init__(self):
         self.API_URL = 'https://www.bungie.net/Platform/Destiny'
-        self.MEMBERSHIP_TYPE = config['MEMBERSHIP_TYPE']  # XBOX = 1, PS = 2
         self.REQUEST_HEADERS = config['REQUEST_HEADERS']
 
     def _request(self, query):
         req = requests.get(self.API_URL+query, headers=self.REQUEST_HEADERS)
         return req.json()
 
-    def get_membership_id_by_display_name(self, display_name):
-        query = '/%d/Stats/GetMembershipIdByDisplayName/%s' % (self.MEMBERSHIP_TYPE,
+    def get_membership_id_by_display_name(self, membership_type, display_name):
+        query = '/%d/Stats/GetMembershipIdByDisplayName/%s' % (membership_type,
                                                                display_name)
-        response = self._request(query)
-        return response['Response']
+        data = self._request(query)
+        return data['Response']
 
-    def get_account_info(self, membership_id):
-        query = '/%d/Account/%s/' % (self.MEMBERSHIP_TYPE, membership_id)
-        response = self._request(query)
-        return response
+    def get_account_info(self, membership_type, membership_id):
+        query = '/%d/Account/%s/' % (membership_type, membership_id)
+        data = self._request(query)
+        return data
 
 if __name__ == '__main__':
     api = DestinyAPI()
-    membership_id = api.get_membership_id_by_display_name('ermff')
+    membership_type = 1  # XBOX: 1, PS: 2
+    membership_id = api.get_membership_id_by_display_name(membership_type,
+                                                          'ermff')
     print("MEMBERSHIP ID: %s" % membership_id)
-    account_info = api.get_account_info(membership_id)
+    account_info = api.get_account_info(membership_type, membership_id)
     print("ACCOUNT INFO: %s" % account_info)
