cddd8f3ded8e777c0d4b0dba581a9e798e9d9c4a
erm <j@ermff.com>
2015-02-28 19:55:14 +1100

    added some more properties, fixing up acitivty info

--- destiny.py
+++ destiny.py
@@ -11,41 +11,75 @@ class DestinyAPI(object):
         self.PLATFORM = platform
         self.USERNAME = username
         self.membership_id()
+        self.characters()
 
-    def _request(self, query):
-        req = requests.get(self.API_URL+query, headers=self.REQUEST_HEADERS)
+    def _request(self, qry):
+        req = requests.get(self.API_URL+qry, headers=self.REQUEST_HEADERS)
         return req.json()
 
     def membership_id(self):
-        query = '/%d/Stats/GetMembershipIdByDisplayName/%s' % (self.PLATFORM,
-                                                               self.USERNAME)
-        data = self._request(query)
-        self.MEMBERSHIP_ID = data['Response']
+        qry = '/%d/Stats/GetMembershipIdByDisplayName/%s' % (self.PLATFORM,
+                                                             self.USERNAME)
+        data = self._request(qry)
+        self.MEMBERSHIP = data['Response']  # will return 0 if invalid
+
+    def characters(self):
+        _characters = self.account_info['Response']['data']['characters']
+        self.CHARACTERS = [DestinyCharacter(self, i) for i in _characters]
 
     @property
     def account_info(self):
-        query = '/%d/Account/%s/' % (self.PLATFORM, self.MEMBERSHIP_ID)
-        data = self._request(query)
+        qry = '/%d/Account/%s/' % (self.PLATFORM, self.MEMBERSHIP)
+        data = self._request(qry)
         return data
 
 
 class DestinyCharacter(object):
 
-    def __init__(self, api, character_id):
-        self.api = api
-        self.CHARACTER_ID = character_id
+    def __init__(self, api, character_info):
+        self.API = api
+        self.CHARACTER_INFO = character_info
+        self.activity_info()
+        self.character_hashes = {'Titan': 3655393761, 'Warlock': 2271682572,
+                                 'Hunter': 671679327}
+
+    def activity_info(self):
+        qry = '/%d/Account/%s/Character/%s/Activities/'
+        qry = qry % (self.API.PLATFORM, self.API.MEMBERSHIP,
+                     self.CHARACTER_INFO['characterBase']['characterId'])
+        self.ACTIVITY_INFO = self.API._request(qry)
 
     @property
-    def activity_info(self):
-        query = '/%d/Account/%s/Character/%s/Activities/' % (self.api.PLATFORM,
-                                                             self.api.MEMBERSHIP_ID,
-                                                             self.CHARACTER_ID)
-        data = self._request(query)
-        return data
+    def character_id(self):
+        return CHARACTER_INFO['characterBase']['characterId']
+
+    @property
+    def base_character_level(self):
+        return self.CHARACTER_INFO['base_character_level']
+
+    @property
+    def character_class(self):
+        return character_hashes[CHARACTER_INFO['characterBase']['classHash']]
+
+    @property
+    def date_last_played(self):
+        return CHARACTER_INFO['characterBase']['dateLastPlayed']
+
+    @property
+    def grimoire_score(self):
+        return CHARACTER_INFO['characterBase']['grimoireScore']
+
+    @property
+    def grimoire_score(self):
+        return CHARACTER_INFO['characterBase']['grimoireScore']
+
+    @property
+    def light_level(self):
+        return CHARACTER_INFO['characterBase']['powerLevel']
 
 if __name__ == '__main__':
     platform = 1  # XBOX: 1, PS: 2
     username = 'ermff'
     api = DestinyAPI(platform, username)
-    characters = api.account_info['Response']['data']['characters']
-    print(characters)
+    for i in api.CHARACTERS:
+        pprint(i.ACTIVITY_INFO)
